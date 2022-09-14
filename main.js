89e9af8d11d6bfbea46452af97f093a0f1406312
Bludidi <bludidi5@gmail.com>
2022-09-14 15:10:07 +0200

    added new JS features

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
 const projects = [
-    project1 {
-        name: 
-    }
+    {
+        name: "assets/images/img_text1.svg",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+        image: "assets/images/img_card1.svg",
+        technologies: ["assets/images/Tags.svg", "assets/images/Tags1.svg"],
+        seeLive: "https://bludidi.github.io/Portfolio-Desktop-version-/", 
+        seeSource: "https://github.com/Bludidi/Portfolio-Desktop-version-" 
+    },
+
+    {
+        name: "assets/images/img_text2.svg",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+        image: "assets/images/img2",
+        technologies: ["assets/images/Tags.svg", "assets/images/Tags1.svg"],
+        seeLive: "https://bludidi.github.io/Portfolio-Desktop-version-/", 
+        seeSource: "https://github.com/Bludidi/Portfolio-Desktop-version-" 
+    },
+
+    {
+        name: "assets/images/img_text3.svg",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+        image: "assets/images/img3.svg",
+        technologies: ["assets/images/Tags.svg", "assets/images/Tags1.svg"],
+        seeLive: "https://bludidi.github.io/Portfolio-Desktop-version-/", 
+        seeSource: "https://github.com/Bludidi/Portfolio-Desktop-version-" 
+    },
+
+    {
+        name: "assets/images/img_text4.svg",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+        image: "assets/images/img4.svg",
+        technologies: ["assets/images/Tags.svg", "assets/images/Tags1.svg"],
+        seeLive: "https://bludidi.github.io/Portfolio-Desktop-version-/", 
+        seeSource: "https://github.com/Bludidi/Portfolio-Desktop-version-" 
+    },
 ];
