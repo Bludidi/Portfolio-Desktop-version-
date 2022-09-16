b020dc1e955a5936697ae8e497f3fef826bdf484
Bludidi <bludidi5@gmail.com>
2022-09-16 22:18:29 +0200

    js file

--- validate.js
+++ validate.js
@@ -16,9 +16,15 @@
 
 
 
-
+const alert = document.querySelector('.alert');
+const footer = document.querySelector('.footer')
 const submit = document.querySelector('.submit');
 const reg = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+
+submit.addEventListener('click', show);
+function show() {
+  alert.style.display = 'block';
+}
 
 const validateEmail = (email) => {
   if (email.match(reg)) {
@@ -31,7 +37,7 @@ const validateEmail = (email) => {
 submit.addEventListener('click', (event) => {
   const validEmail = document.getElementById('mail').value;
   if (!validateEmail(validEmail)) {
-    document.querySelector('.error').innerHTML = 'Invalid email! \n Please use lowercase!';
+    document.querySelector('.error').innerHTML = 'Invalid email! Please use lowercase!';
     event.preventDefault();
   } else {
     document.querySelector('.error').innerHTML = 'Submited Successfully!';
