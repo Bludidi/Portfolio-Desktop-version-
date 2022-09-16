bc5944b69bf8461c82408a367b9f290b0738dc81
Bludidi <bludidi5@gmail.com>
2022-09-16 19:43:25 +0200

    fixed linter errors

--- validate.js
+++ validate.js
@@ -19,22 +19,21 @@
 
 const submit = document.querySelector('.submit');
 const reg = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
-	
 
-	const validateEmail = (email) => {
-	  if (email.match(reg)) {
-	    return true;
-	  }
-	  return false;
-	};
-	
+const validateEmail = (email) => {
+  if (email.match(reg)) {
+    return true;
+  }
+  return false;
+};
 
-	submit.addEventListener('click', (event) => {
-	  const validEmail = document.getElementById('mail').value;
-	  if (!validateEmail(validEmail)) {
-	    document.querySelector('.error').innerHTML = 'Invalid email! \n Please use lowercase!';
-	    event.preventDefault();
-	  } else {
-	    document.querySelector('.error').innerHTML = 'Submited Successfully!';
-	  }
-	});
+
+submit.addEventListener('click', (event) => {
+  const validEmail = document.getElementById('mail').value;
+  if (!validateEmail(validEmail)) {
+    document.querySelector('.error').innerHTML = 'Invalid email! \n Please use lowercase!';
+    event.preventDefault();
+  } else {
+    document.querySelector('.error').innerHTML = 'Submited Successfully!';
+  }
+});
