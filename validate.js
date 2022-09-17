cc492f31231c32e383cfd29e570e09fdcb625d08
Bludidi <bludidi5@gmail.com>
2022-09-17 11:24:54 +0200

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,20 +1,3 @@
-// const email = document.getElementById("mail").value; 
-// const form = document.getElementById('form');
-// const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-// email.addEventListener('submit', function(event){
-//   event.preventDefault()
-// });
-
-// function validateEmail(email){
-//   if(reg.test(string(email).toLowerCase())!==true){
-//     console.log('Submitted successfully!');
-//   } else
-//   console.log('Invalid email! \n Please use lowercase!')
-// }
-
-
-
-
 
 const alert = document.querySelector('.alert');
 const footer = document.querySelector('.footer')
